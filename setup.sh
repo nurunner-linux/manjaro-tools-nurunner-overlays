f7c819c36c1fa3d9caa42f8b3d5a661bde343b4f
Matt Kowal <matt@mkxt.net>
2017-02-26 22:32:11 -0500

    added simple setup script

---
+++ setup.sh
@@ -0,0 +1,8 @@
+ sudo rm -rf /home/nurunner/manjaro-tools-iso-profiles/*
+ sudo rm -rf /etc/manjaro-tools/*
+ sudo rm -rf /usr/share/manjaro-tools/*
+
+ sudo cp -r home/nurunner/* ~
+ sudo cp -r etc/manjaro-tools/* /etc/manjaro-tools
+ sudo cp -r usr/share/manjaro-tools/* /usr/share/manjaro-tools
+ 
