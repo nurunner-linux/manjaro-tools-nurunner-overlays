f51ec12339d9de6850584f5cd7ee2ca1e0e704b3
Matt Kowal <matt@mkxt.net>
2017-03-05 12:07:27 -0500

    fix setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,9 @@
+ sudo rm -rf /home/nurunner/.config/manjaro-tools/*
  sudo rm -rf /home/nurunner/manjaro-tools-iso-profiles/*
  sudo rm -rf /etc/manjaro-tools/*
  sudo rm -rf /usr/share/manjaro-tools/*
 
+ sudo cp -r /home/nurunner/.config/manjaro-tools/* ~/.config/manjaro-tools
  sudo cp -r home/nurunner/* ~
  sudo cp -r etc/manjaro-tools/* /etc/manjaro-tools
  sudo cp -r usr/share/manjaro-tools/* /usr/share/manjaro-tools
