139aae6d10831bb70d0405f2fb90fa085a63b092
Matt Kowal <matt@mkxt.net>
2017-03-27 11:35:02 -0400

    new manjaro-tools 15 overlays

--- setup.sh
+++ setup.sh
@@ -1,10 +1,10 @@
- sudo rm -rf /home/nurunner/.config/manjaro-tools/*
- sudo rm -rf /home/nurunner/manjaro-tools-iso-profiles/*
+ sudo rm -rf /home/kowal/.config/manjaro-tools/* 
+ sudo rm -rf /home/kowal/manjaro-tools-iso-profiles/*
  sudo rm -rf /etc/manjaro-tools/*
  sudo rm -rf /usr/share/manjaro-tools/*
 
- sudo cp -r /home/nurunner/.config/manjaro-tools/* ~/.config/manjaro-tools
- sudo cp -r home/nurunner/* ~
+ sudo cp -r home/* ~
+ sudo cp -r home/.config/* ~/.config
  sudo cp -r etc/manjaro-tools/* /etc/manjaro-tools
  sudo cp -r usr/share/manjaro-tools/* /usr/share/manjaro-tools
  
